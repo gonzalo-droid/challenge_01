af64b45a33268a990ffb1780ca946482fe6c095c
Gonzalo <gonzalolopezguerrero2050@gmail.com>
2021-10-05 13:47:29 -0500

    boton para especificar ubicacion

--- app/src/main/java/com/example/map_trips/view/fragment/MapFragment.kt
+++ app/src/main/java/com/example/map_trips/view/fragment/MapFragment.kt
@@ -1,11 +1,17 @@
 package com.example.map_trips.view.fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import com.example.map_trips.R
@@ -20,15 +26,20 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MapFragment : Fragment(), OnMapReadyCallback , GoogleMap.OnMarkerClickListener {
+class MapFragment : Fragment(), OnMapReadyCallback , GoogleMap.OnMarkerClickListener,
+    GoogleMap.OnMyLocationButtonClickListener, // boton de ubicacion
+    GoogleMap.OnMyLocationClickListener  // coordenadas
+{
     // TODO: Rename and change types of parameters
 
     private lateinit var map:GoogleMap
 
+    companion object{
+        const val REQUEST_CODE_LOCATION=0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -50,10 +61,18 @@ class MapFragment : Fragment(), OnMapReadyCallback , GoogleMap.OnMarkerClickList
     override fun onMapReady(googleMap: GoogleMap) {
         // se lleva cuando el mapa se ha creado
         map = googleMap
+        createMarkers()
 
+        map.setOnMyLocationButtonClickListener(this)
+
+        enableLocation()
+    }
+
+    private fun createMarkers() {
+        // lista de las ubicaciones
         var list_ubication = UbicationList.getListUbications();
 
-
+        // recorre las ubicaciones y asigna los datos para la creacion del market
         for (list in list_ubication){
             val gps = LatLng(list.latitude, list.longitude)
             val market = MarkerOptions().position(gps).title(list.name)
@@ -61,19 +80,96 @@ class MapFragment : Fragment(), OnMapReadyCallback , GoogleMap.OnMarkerClickList
 
         }
 
-        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(
+        // zoom por defecto a una coordenada
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(
             LatLng(-6.769792136313882,-79.85495851696817)
             , 8f))
 
-
+        // listener al hacer clic en el market
         map?.setOnMarkerClickListener(this)
-
     }
 
     override fun onMarkerClick(marker: Marker): Boolean {
+
+        // Informacion del marker a UbicationDetailFragment
         var name = marker.title
         val bundle = bundleOf(Pair("name",name))
         findNavController().navigate(R.id.ubicationDetailFragment, bundle)
         return true
+    }
+
+
+    private fun isLocationPermissionGranted() =
+        ContextCompat.checkSelfPermission(requireActivity(),
+        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun enableLocation(){
+        // si no esta inicializada return
+        if(!::map.isInitialized) return
+        // permiso activado
+        if(isLocationPermissionGranted()){
+            map.isMyLocationEnabled = true
+        }else{
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+        )){
+            Toast.makeText(requireActivity(), "Activa los permisos de GPS", Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty()  && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                map.isMyLocationEnabled = true
+            }else{
+                Toast.makeText(requireActivity(), "Activa los permisos de GPS", Toast.LENGTH_SHORT).show()
+            }
+
+            else -> {}
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        if(!::map.isInitialized) return
+        if(!isLocationPermissionGranted()){
+            map.isMyLocationEnabled = false
+            Toast.makeText(requireActivity(), "Activa los permisos de GPS", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            map.isMyLocationEnabled = false
+            Toast.makeText(requireActivity(), "Activa los permisos de GPS", Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+        TODO("Not yet implemented")
     }
 }
